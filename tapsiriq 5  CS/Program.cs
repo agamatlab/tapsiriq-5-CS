9fe0dc27405084b95fc7dea67a9fec4a264ec1e3
agha <icloudakberzade@gmail.com>
2022-04-14 12:24:15 +0400

    Added more Examples.

--- tapsiriq 5  CS/Program.cs
+++ tapsiriq 5  CS/Program.cs
@@ -7,8 +7,11 @@ class Program
     {
         PetShop petShop = new PetShop();
         petShop.AddElement(new Cat("Milo", 4, Gender.Female, Convert.ToSByte(60), 8.0m, 4));
+        petShop.AddElement(new Cat("MiloTemp", 4, Gender.Female, Convert.ToSByte(60), 8.0m, 4));
         petShop.AddElement(new Dog("Peroho", 2, Gender.Male, Convert.ToSByte(40), 2.0m, 8));
         petShop.AddElement(new Bird("Pedro", 3, Gender.Male, Convert.ToSByte(80), 2.3m, 1, true));
+
+        petShop.RemoveByNickName("MiloTemp");
 
         Console.WriteLine("Starting...");
         Thread.Sleep(2000);
